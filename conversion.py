61fedbff2d851e085b3809868487f4f21b13dea9
Alejandro García <gar231136@uvg.edu.gt>
2024-03-03 21:45:35 -0600

    Finalización de conversiones

--- conversion.py
+++ conversion.py
@@ -78,6 +78,98 @@ def convertFraction( frac):
         decimal += int(frac[i]) * (2 ** -(i + 1))
     return decimal
 
+#Roberto Nájera - Deicmal a Binario IEEE 754
+def FloatToBinary():
+    S = ""
+    EXP = ""
+    Frac = ""
+
+    print("\nIngrese el número decimal que desea convertir a binario IEEE 754: ")
+    Float = input()
+
+    # separa el signo
+    if (Float.startswith("-")):
+        S = "1"
+    else:
+        S = "0"
+
+    # verifica que el número es un decimal
+    try:
+        Float = abs(float(Float))
+    except:
+        return "* Error. No es un decimal representable"
+
+    zeros = 0
+    Int = int(Float) # Se puede usar la función de decimal a binario aquí
+    if Int == 0:
+        zerosindecimal= True
+        zeros += 1
+    else:
+        zerosindecimal = False
+    Int = str(format(Int, "b"))
+
+    # convierte el decimal a binario
+    decimal = ""
+    quotient = Float - int(Float)
+    zerosafter126 = 0
+    while quotient > 0 and len(decimal) + len(Int) - 1 + zerosafter126 - zeros < 23:
+        quotient *= 2
+        if (quotient >= 1):
+            decimal += "1"
+            quotient -= 1
+            zerosindecimal = False
+        else:
+            decimal += "0"
+            if zerosindecimal:
+                zeros += 1
+                if zeros > 126:
+                    zerosafter126 += 1
+
+
+    
+    #print(Int + "." + decimal)
+
+    # Construye el exponente
+    if zeros > 0:
+        exp = 127 - zeros
+        if (exp < 0):
+            exp = 0
+    else:
+        exp = len(Int) - 1 + 127
+        if exp >= 255:
+            exp = 255
+            print( "∞ infinity")
+
+    exp = str(format(exp, "b"))
+    for i in range(8 - len(exp)):
+        EXP += "0"
+
+    EXP += exp
+
+    # Construye la fracción
+    frac = Int + decimal
+    if zeros > 0:
+        frac = frac.partition("0" * (zeros - zerosafter126))[2]
+        for i in range(23):
+            if i >= len(frac) - 1:
+                Frac += "0"
+            else:
+                Frac += frac[i +  1]
+    else:
+        for i in range(23):
+            if i >= len(frac) - 1:
+                Frac += "0"
+            else:
+                Frac += frac[i + 1]
+
+        if Float >= 2**129 - 2**105:
+            Frac = "1"*23
+
+    if EXP.find("1") == -1 and Frac.find("1") == -1:
+        print("0 empty")
+    
+    print(" * El número binario en IEEE754 es: "+ S + " " + EXP + " " + Frac)
+    
 
 print("CONVERSIONES - HOJA DE TRABAJO 5")
 while True:
@@ -96,10 +188,7 @@ while True:
         binario_a_decimal()
 
     elif opcion == '3':
-        decimal = float(input("Ingrese el número decimal que desea convertir a binario IEEE 754: "))
-        # Implementación para convertir decimal a binario IEEE 754
-        resultado = bin(decimal)
-        print("El resultado es:", resultado)
+        FloatToBinary()
     elif opcion == '4':
         convertToIEE754()
     elif opcion == '5':
